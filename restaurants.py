aae12bbf1b79ed63269801e54e2a1bbfa1add5dc
FD2906 <frank.dong2906@gmail.com>
2022-01-21 23:30:01 +0000

    experimenting with csv code

--- restaurants.py
+++ restaurants.py
@@ -1,4 +1,20 @@
 import csv
 
 
+# csv file experiment code #
+tapas_restaurants = []
+with open('spreadsheet.csv', newline = '') as spreadsheet:
+    spreadsheet_reader = csv.DictReader(spreadsheet)
+    for row in spreadsheet_reader:
+        tapas_restaurants.append(row['Tapas'])
+
+for restaurant in tapas_restaurants:
+    if restaurant == '':
+        break
+    print(restaurant)
+
+
+
+
+
 
